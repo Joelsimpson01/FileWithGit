1866b1d1e0f32acd89310f4ee91a1222ec37dc15
Joel Simpson <270112350@yoobeestudent.ac.nz>
2021-11-22 13:59:36 +1300

    First

---
+++ FilesWithGit/FilesWithGit.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+
+struct shoes {
+    string brand;
+    string material;
+    string color;
+    int size;
+    float price;
+};
+
+
+int main()
+{
+    
+}
+
